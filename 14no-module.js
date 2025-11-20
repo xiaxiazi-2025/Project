987314b04d8235a77e5fd4af909fefa57afde8f6
xiaxiazi-2025 <1782685949@qq.com>
2025-11-20 23:34:21 +0800

    14no-module.js

---
+++ 14no-module.js
@@ -0,0 +1,6 @@
+function add(a,b) {return a + b}
+function multiply(a,b) {return a * b}
+const result1 = add(2,3);
+const result2 = multiply(4,5);
+console.log('加法结果：',result1);
+console.log('乘法结果：',result2);
